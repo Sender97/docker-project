e405fe546847ca65d806dfcecc93ddcc1c534da8
Konrad Senderowski <sseder@wp.pl>
2023-02-15 21:51:02 +0100

    corrected files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          assdasdasdas
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          suki1
+          Learn React
         </a>
       </header>
     </div>
